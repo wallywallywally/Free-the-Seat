f531eaa2fc33fae3cfbd27888038de4bc2ceec8c
samuelory <samuelong7@gmail.com>
2023-06-15 16:35:38 +0800

    query database every 10 secs

--- arduino/app.mjs
+++ arduino/app.mjs
@@ -1,7 +1,5 @@
 // JavaScript source code
 import { SerialPort} from 'serialport'
-//import { createClient } from '@supabase/supabase-js'
-//var SerialPort = require("serialport");
 import { createClient } from '@supabase/supabase-js'
 
 
@@ -9,8 +7,25 @@ const supabase = createClient(
     "https://agsfacuitdbjrlthotrv.supabase.co", 
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc2ZhY3VpdGRianJsdGhvdHJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyMTExMTMsImV4cCI6MjAwMTc4NzExM30.pqHLZrrIzMQgM-BSsCaIzcodSgfaYtvIDmwxDYENRYE"
     )
-console.log("hello world!")
+async function main () {
+    
+    let { data: seats, error } = await supabase.from("seats").select('*');
+console.log(seats)
 
+}
+setInterval(main, 10000)
+
+// supabase
+// .from("seats")
+// .select()
+// .then((data) => {
+//     console.log("success")
+//     console.log(data)
+// })
+// .catch((error) => {
+//     console.log("fail")
+//   console.log(JSON.stringify(error))
+// });
 //  var arduinoCOMPort = "COM3";
 
 // var arduinoSerialPort = new SerialPort(arduinoCOMPort, {
