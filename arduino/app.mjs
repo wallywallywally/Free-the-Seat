f25379ede176776ec7bd9639d31db890bb4f22a4
samuelory <samuelong7@gmail.com>
2023-06-15 20:20:52 +0800

    continued arduino stff

--- arduino/app.mjs
+++ arduino/app.mjs
@@ -7,11 +7,15 @@ const supabase = createClient(
     "https://agsfacuitdbjrlthotrv.supabase.co", 
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc2ZhY3VpdGRianJsdGhvdHJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyMTExMTMsImV4cCI6MjAwMTc4NzExM30.pqHLZrrIzMQgM-BSsCaIzcodSgfaYtvIDmwxDYENRYE"
     )
-async function main () {
-    
-    let { data: seats, error } = await supabase.from("seats").select('*');
-console.log(seats)
 
+let seatslog = []
+async function main () {
+    let { data: seats, error } = await supabase.from("seats").select('*');
+//console.log(seats)
+seats.forEach( (seat) => {
+    console.log(seat.id)
+    console.log(seat.light_status)
+})
 }
 setInterval(main, 10000)
 
